1ce0bce79506fc90c1ac66a3cdd0eceb8f5070be
Eder Lima <devilsouls666@gmail.com>
2022-06-22 00:25:53 -0300

    feat: uptimize server loading with model dump

--- src/server/factories/movie_recommender.py
+++ src/server/factories/movie_recommender.py
@@ -1,15 +1,19 @@
+import joblib, os
 from server.algorithms.movie_recommender import MovieRecommender
-
 
 class MovieRecommenderFactory:
     __instance: MovieRecommender = None
 
     @staticmethod
     def get_instance():
+        if os.path.exists('ml-model.save'):
+            MovieRecommenderFactory.__instance = joblib.load('ml-model.save')
+
         if MovieRecommenderFactory.__instance is None:
             MovieRecommenderFactory.__instance = MovieRecommender(
                 "movies.csv",
                 "ratings.csv",
             )
+            joblib.dump(MovieRecommenderFactory.__instance, 'ml-model.save')
 
         return MovieRecommenderFactory.__instance
